f80edd26514db8a92c02bcaf0cbf5f4deac3d22a
CareyFrancis <carexfm@gmail.com>
2018-12-03 15:39:39 +0300

    Made changes to the README file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 var trySide = function() {
-  var side1 = document.getElementById('input1').value;
-  var side2 = document.getElementById('input2').value;
-  var side3 = document.getElementById('input3').value;
+    var side1 = document.getElementById('input1').value;
+    var side2 = document.getElementById('input2').value;
+    var side3 = document.getElementById('input3').value;
 
-  var str1 = parseFloat(side1);
-  var str2 = parseFloat(side2);
-  var str3 = parseFloat(side3);
+    var str1 = parseFloat(side1);
+    var str2 = parseFloat(side2);
+    var str3 = parseFloat(side3);
 
-  if (str1===str2 && str1===str3 && str2===str3) {
-    alert("This is an Equilateral Triangle.");
-  } else if (str1===str2 || str1===str3 || str2===str3) {
-    alert ("This is an Isosceles Triangle.");
-  } else if (str1!==str2 || str1!==str3 || str2!==str3) {
-    alert ("This is a Scalene Trinagle.");
-  } else (sum(str1 ,str2)=<str3 || sum(str1, str3)=< str2 || sum(str2, str3)=<str1){
-    alert ("This is NOT a triangle");
-  }
-}
+    if (str1 === str2 && str1 === str3 && str2 === str3) {
+      alert("This is an Equilateral Triangle.");
+    } else if (str1 === str2 || str1 === str3 || str2 === str3) {
+      alert("This is an Isosceles Triangle.");
+    } else if (str1 !== str2 || str1 !== str3 || str2 !== str3) {
+      alert("This is a Scalene Trinagle.");
+    } else {
+      alert("This is NOT a triangle.");
+    }
