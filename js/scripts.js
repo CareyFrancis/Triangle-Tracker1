76f57ff357b24119568b6968118d50d54b9cba98
CareyFrancis <carexfm@gmail.com>
2018-12-03 10:01:26 +0300

    Created new variables str1,str2 and str3 in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var trySide = function() {
-  var side1 = document.getElementById() 
-  var side2 =
-  var side3 =
+  var side1 = document.getElementById('input1').value;
+  var side2 = document.getElementById('input2').value;
+  var side3 = document.getElementById('input3').value;
+
+  var str1 = side1.toString();
+  var str2 = side2.toString();
+  var str3 = side3.toString();
 }
