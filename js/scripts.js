f0cc28bab13c75aaa99780629a469224dbc4cd00
CareyFrancis <carexfm@gmail.com>
2018-12-03 15:27:31 +0300

    Added line breaker tags to space out the input boxes

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ var trySide = function() {
   var side2 = document.getElementById('input2').value;
   var side3 = document.getElementById('input3').value;
 
-  var str1 = side1.toString();
-  var str2 = side2.toString();
-  var str3 = side3.toString();
+  var str1 = parseFloat(side1);
+  var str2 = parseFloat(side2);
+  var str3 = parseFloat(side3);
 
   if (str1===str2 && str1===str3 && str2===str3) {
     alert("This is an Equilateral Triangle.");
