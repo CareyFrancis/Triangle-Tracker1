268d6dd9881ebbbd1c08983a3e305ea668628e89
CareyFrancis <carexfm@gmail.com>
2018-12-03 11:08:33 +0300

    Added if else statements to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,14 @@ var trySide = function() {
   var str1 = side1.toString();
   var str2 = side2.toString();
   var str3 = side3.toString();
+
+  if (str1===str2 && str1===str3 && str2===str3) {
+    alert("This is an Equilateral Triangle.");
+  } else if (str1===str2 || str1===str3 || str2===str3) {
+    alert ("This is an Isosceles Triangle.");
+  } else if (str1!==str2 || str1!==str3 || str2!==str3) {
+    alert ("This is a Scalene Trinagle.");
+  } else (sum(str1 ,str2)=<str3 || sum(str1, str3)=< str2 || sum(str2, str3)=<str1){
+    alert ("This is NOT a triangle");
+  }
 }
