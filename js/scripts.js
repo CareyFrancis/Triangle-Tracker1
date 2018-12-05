4273b8cb78a27c836305d868b838faa2db5e0538
CareyFrancis <carexfm@gmail.com>
2018-12-05 14:54:40 +0300

    edit function

--- js/scripts.js
+++ js/scripts.js
@@ -19,37 +19,3 @@ var trySide = function() {
       alert("INVALID OPERATION!")
     }
 };
-/*function answer(){
-var checker=[];
-checker.push(parseFloat(prompt("Enter side A: ")));
- if (isNaN(checker[0])=== true){
-   alert("Please enter a number in the field");
- }else{
- checker.push(parseFloat(prompt("Enter side B: ")));
-   if (isNaN(checker[1])=== true){
-       alert("Please enter a number in the field");
-     }else{
-     checker.push(parseFloat(prompt("Enter side C: ")));
-       if (isNaN(checker[2])=== true){
-           alert("Please enter a number in the field");
-       }else{
-       if((checker[0]+checker[1])<=checker[2] || (checker[1]+checker[2])<=checker[0] || (checker[0]+checker[2])<=checker[1]){
-           alert("Your shape is Not a Triangle");
-       }
-       else if(checker[0]===checker[1] && checker[0]===checker[2] && checker[1]===checker[2]){
-           alert("Your shape is Equilateral");
-       }
-       else if(checker[0]===checker[1] || checker[1]===checker[2] || checker[0]===checker[2]){
-           alert("Your shape is Isosceles");
-       }
-       else if((checker[0]+checker[1])>=checker[2] || (checker[1]+checker[2])>=checker[0] || (checker[0]+checker[2])>=checker[1]){
-           alert("Your shape is Scalene");
-       }
-       else{
-           alert("Value entered must be a number");
-       }
-     }
-   }
- }
-};
-*/
