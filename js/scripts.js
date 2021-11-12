aa52747fcb5ed9c1cdf0719b647b080b272bec60
Carey Ojall <45191740+CareyFrancis@users.noreply.github.com>
2021-11-12 17:06:34 +0300

    Create scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@ checker.push(parseFloat(prompt("Enter side A: ")));
        alert("Please enter a number in the field");
      }else{
      checker.push(parseFloat(prompt("Enter side C: ")));
-       if (isNaN(checker[2])=== true){*/
+       if (isNaN(checker[2])=== true){
            alert("Please enter a number in the field");
        }else{
        if((checker[0]+checker[1])<=checker[2] || (checker[1]+checker[2])<=checker[0] || (checker[0]+checker[2])<=checker[1]){
@@ -55,3 +55,4 @@ checker.push(parseFloat(prompt("Enter side A: ")));
  }
 };
 
+*/
